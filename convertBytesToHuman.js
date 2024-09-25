b269d2f7740caaf22eaa41cc0d1f1420b7155383
Yuliya Kudashkina <kudashkina-yuliya@mail.ru>
2024-09-25 07:57:31 +0300

    # Changes to be committed:

---
+++ convertBytesToHuman.js
@@ -0,0 +1,43 @@
+/*
+ * В этом задании надо разработать функцию
+ * `convertBytesToHuman`. Эта функция  должна принимать
+ * аргумент `bytes` только числового типа.
+ * На выходе функция должна отдать
+ * человекопонятную строку, которая будет
+ * отражать размер файла. Округление, максимум,
+ * до 2 знаков после запятой, исключая нули.
+ *  Примеры использования:
+ * `convertBytesToHuman(1024) === '1 KB';`
+ * `convertBytesToHuman(123123123) === '117.42 MB';`
+ * `convertBytesToHuman(1610612736) === '1.5 GB';`
+ * Необходимо предусмотреть защиту от
+ * передачи аргументов неправильного типа
+ * и класса (например, отрицательные числа).
+ * В случае передачи неподходящего аргумента,
+ * функция должна вернуть false.
+ */
+
+function convertBytesToHuman(bytes) {
+ if (typeof bytes !== 'number' || bytes < 0 || isNaN(bytes)) {
+    return false;
+ } 
+
+ if (bytes === 0) return '0 B';
+
+ const size = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+ const i = Math.floor(Math.log(bytes)/Math.log(1024));
+
+ const result = bytes / Math.pow(1024, i);
+
+ return `${parseFloat(result.toFixed(2))} ${size[i]}`;
+}
+
+
+export default convertBytesToHuman;
+
+// console.log(convertBytesToHuman(1024)); // '1 KB'
+// console.log(convertBytesToHuman(123123123)); // '117.42 MB'
+// console.log(convertBytesToHuman(1610612736)); // '1.5 GB'
+// console.log(convertBytesToHuman(-123)); // false
+// console.log(convertBytesToHuman('string')); // false
+// console.log(convertBytesToHuman(0)); // '0 B'
---
+++ correctSentence.js
@@ -0,0 +1,36 @@
+/*
+For the input of your function, you will be given one sentence.
+You have to return a corrected version,
+that starts with a capital letter and ends with a period (dot).
+
+Example:
+
+input (string): "hey, friend"
+output (string): "Hey, friend."
+
+Updated first 'h' to 'H', added '.'.
+
+More examples:
+
+correctSentence("greetings, friends") == "Greetings, friends."
+correctSentence("Greetings, friends") == "Greetings, friends."
+correctSentence("Greetings, friends.") == "Greetings, friends."
+ */
+
+function correctSentence(text) {
+  const capitalizedText = text.charAt(0).toUpperCase() + text.slice(1);
+
+  if (capitalizedText.charAt(capitalizedText.length - 1) !== '.'){
+    return capitalizedText + '.';
+  }
+  
+  return capitalizedText;
+}
+
+
+export default correctSentence;
+
+// console.log(correctSentence("greetings, friends")); // "Greetings, friends."
+// console.log(correctSentence("Greetings, friends")); // "Greetings, friends."
+// console.log(correctSentence("Greetings, friends.")); // "Greetings, friends."
+
